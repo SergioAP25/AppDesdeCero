adf611c8637390c18991c52ca2a050ffaa1ace39
Awoken Infinity <Sergio25Andrade50@gmail.com>
2023-04-17 22:17:20 +0200

    Test commit for another pc

--- app/src/main/java/com/sergio/appdesdecero/ui/view/PokemonViewHolder.kt
+++ app/src/main/java/com/sergio/appdesdecero/ui/view/PokemonViewHolder.kt
@@ -1,5 +1,6 @@
 package com.sergio.appdesdecero.ui.view
 
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.sergio.appdesdecero.databinding.PokemonCellBinding
