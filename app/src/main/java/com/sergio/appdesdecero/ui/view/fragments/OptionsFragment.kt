bc5ea746a97dbc699e50f1eda8e3e05c5e11399d
Awoken Infinity <Sergio25Andrade50@gmail.com>
2023-04-23 21:33:32 +0200

    Upgraded UI

--- app/src/main/java/com/sergio/appdesdecero/ui/view/fragments/OptionsFragment.kt
+++ app/src/main/java/com/sergio/appdesdecero/ui/view/fragments/OptionsFragment.kt
@@ -2,6 +2,7 @@ package com.sergio.appdesdecero.ui.view.fragments
 
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.getIntent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,6 +13,7 @@ import com.sergio.appdesdecero.R
 import com.sergio.appdesdecero.databinding.FragmentOptionsBinding
 import com.sergio.appdesdecero.ui.view.LoginActivity
 import com.sergio.appdesdecero.ui.view.MainActivity
+import com.sergio.appdesdecero.ui.view.ProviderType
 import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class OptionsFragment : Fragment() {
@@ -22,13 +24,16 @@ class OptionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentOptionsBinding.inflate(layoutInflater)
-
-        initUI()
+        val email = activity?.intent?.extras?.getString("email", null)
+        val provider = activity?.intent?.extras?.getString("provider", null)
+        initUI(email ?: "", provider ?: "")
 
         return binding.root
     }
 
-    private fun initUI(){
+    private fun initUI(email: String, provider: String){
+        binding.email.text = email
+        binding.provider.text = provider
         binding.logOutButton.setOnClickListener {
             val prefs = getActivity()?.getSharedPreferences("PokeSearch", Context.MODE_PRIVATE)?.edit()
             prefs?.clear()
